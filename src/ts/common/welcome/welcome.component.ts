7b9210a11e037f8925bb9cd1ff337ab6931a2925
alexjhannan <alexjhannan@gmail.com>
2016-07-07 16:39:13 -0400

    Rearranged the Angular2 Webpack starter towards target architecture

---
+++ src/ts/common/welcome/welcome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'welcome',
+    styleUrls: ['./welcome.component.css'],
+    template: `<div>
+        <h1>Hello from Angular 2 with Webpack</h1>
+        <img src="src/images/angular.png">
+    </div>`
+})
+export class WelcomeComponent {}
