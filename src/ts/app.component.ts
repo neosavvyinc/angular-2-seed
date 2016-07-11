dc811c5557191921089ffc187fa4d0abd52b63c8
alexjhannan <alexjhannan@gmail.com>
2016-07-11 13:18:31 -0400

    Established a stable pattern for piping in CSS & HTML

--- src/ts/app.component.ts
+++ src/ts/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+
 import '../css/styles.css';
 
 import { WelcomeComponent } from './common/welcome/welcome.component';
 
 @Component({
     selector: 'app',
-    styleUrls: ['./app.component.css'],
     directives: [WelcomeComponent],
-    template: `<main>
-        <welcome></welcome>
-    </main>`
+    template:
+        `<main>
+            <welcome></welcome>
+        </main>`
 })
+
 export class AppComponent {}
--- src/ts/common/welcome/welcome.component.ts
+++ src/ts/common/welcome/welcome.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+
 @Component({
     selector: 'welcome',
-    styleUrls: ['./welcome.component.css'],
-    template: `<div>
-        <h1>Hello from Angular 2 with Webpack</h1>
-        <img src="src/images/angular.png">
-    </div>`
+    styles: [require('./welcome.component.css')],
+    template: require('./welcome.component.html')
 })
+
 export class WelcomeComponent {}
